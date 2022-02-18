60caa127ec5db6edcd7c46eed3602ee86d32e56f
amiokuefe <amiokuefe01@gmail.com>
2022-02-18 13:20:02 +0100

    successfully isolated the RevenueSalesCost to its own component

--- src/components/homeDashBoard/HomeDashBoard.jsx
+++ src/components/homeDashBoard/HomeDashBoard.jsx
@@ -1,61 +1,14 @@
 import React from 'react'
 import "./homeDashBoard.css"
 import {ArrowDownward} from '@mui/icons-material';
+import RevenueSalesCost from './revenueSalesCost/RevenueSalesCost';
 
 function HomeDashBoard() {
   return (
     <div className='homeDashBoardContainer'>
+      <RevenueSalesCost />
         
-        <div className='revenueSalesContainer'>
-          <div className='revenueSalesContainerItem'>
-            Revenue
-            <p> <b>$2,454 </b>
-              <span className='indexValue'>-11.4</span> 
-              <span className='arrow'>
-                <ArrowDownward 
-                    color="secondary"
-                    fontSize='small'/>
-              </span> <br/>
-              {/* <span className='footerNote'>Compared to last Month</span> */}
-              
-            </p>
-            Compared to last Month
-
-           </div>{/*End of revenueSalesContainerItem */}
-
-           <div className='revenueSalesContainerItem'>
-            Sales
-            <p> <b>$2,454 </b>
-              <span className='indexValue'>-11.4</span> 
-              <span className='arrow'>
-                <ArrowDownward 
-                    color="secondary"
-                    fontSize='small'/>
-              </span> <br/>
-              {/* <span className='footerNote'>Compared to last Month</span> */}
-              
-            </p>
-            Compared to last Month
-
-           </div>{/*End of revenueSalesContainerItem */}
-
-           <div className='revenueSalesContainerItem'>
-            Cost
-            <p> <b>$2,454 </b>
-              <span className='indexValue'>-11.4</span> 
-              <span className='arrow'>
-                <ArrowDownward 
-                    color="secondary"
-                    fontSize='small'/>
-              </span> <br/>
-              {/* <span className='footerNote'>Compared to last Month</span> */}
-              
-            </p>
-            Compared to last Month
-
-           </div>{/*End of revenueSalesContainerItem */}
-         
-        </div>
+        
         
     </div>
   )
--- src/components/homeDashBoard/revenueSalesCost/RevenueSalesCost.jsx
+++ src/components/homeDashBoard/revenueSalesCost/RevenueSalesCost.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import './revenueSalesCost.css'
+import {ArrowDownward} from '@mui/icons-material';
 
 function RevenueSalesCost() {
   return (
-    <div>RevenueSalesCost</div>
+    <div className='revenueSalesContainer'>
+          <div className='revenueSalesContainerItem'>
+            Revenue
+            <p> <b>$2,454 </b>
+              <span className='indexValue'>-11.4</span> 
+              <span className='arrow'>
+                <ArrowDownward 
+                    color="secondary"
+                    fontSize='small'/>
+              </span> <br/>
+              {/* <span className='footerNote'>Compared to last Month</span> */}
+              
+            </p>
+            Compared to last Month
+
+           </div>{/*End of revenueSalesContainerItem */}
+
+           <div className='revenueSalesContainerItem'>
+            Sales
+            <p> <b>$2,454 </b>
+              <span className='indexValue'>-11.4</span> 
+              <span className='arrow'>
+                <ArrowDownward 
+                    color="secondary"
+                    fontSize='small'/>
+              </span> <br/>
+              {/* <span className='footerNote'>Compared to last Month</span> */}
+              
+            </p>
+            Compared to last Month
+
+           </div>{/*End of revenueSalesContainerItem */}
+
+           <div className='revenueSalesContainerItem'>
+            Cost
+            <p> <b>$2,454 </b>
+              <span className='indexValue'>-11.4</span> 
+              <span className='arrow'>
+                <ArrowDownward 
+                    color="secondary"
+                    fontSize='small'/>
+              </span> <br/>
+              {/* <span className='footerNote'>Compared to last Month</span> */}
+              
+            </p>
+            Compared to last Month
+
+           </div>{/*End of revenueSalesContainerItem */}
+         
+        </div>
   )
 }
 
