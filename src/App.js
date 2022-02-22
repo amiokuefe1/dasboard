a7f4e86909f863e8a367b01708ef7c55e100b514
amiokuefe1 <amiokuefe@gmail.com>
2022-02-22 10:35:31 +0100

    I successfully reorganise the Active ActiveUserDetails MembersPanel and TransactionPanel components into the HomeDashBoard Component as Parent

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import Topbar from "./components/topbar/Topbar";
 import MainSideBar from "./components/mainSideBar/MainSideBar";
 import HomeDashBoard from "./components/homeDashBoard/HomeDashBoard";
 import "./App.css"
-import ActiveUserDetails from "./components/homeDashBoard/activeUserDetails/ActiveUserDetails";
-import MembersPanel from "./components/homeDashBoard/membersTransactionsComponent/membersPanel/MembersPanel";
-import TransactionPanel from "./components/homeDashBoard/membersTransactionsComponent/transactionPanel/TransactionPanel";
+
 
 function App() {
   return (
@@ -13,18 +11,10 @@ function App() {
       <Topbar />
       
       <div className="container">
-        {/* <div className="left">
-          hello
-        </div> */}
         <MainSideBar />
         
         <div className="right">
           <HomeDashBoard />
-          <ActiveUserDetails/>
-          <div className="userTransact">
-            <MembersPanel/>
-            <TransactionPanel />
-          </div>
           
           
         </div>
